d08a3e373ba6c3da0989f81b424033f1a355cc4f
inhooo00 <chldlsgh0987@naver.com>
2024-06-25 00:39:24 +0900

    Feat: Review 엔티티 생성

---
+++ Skhuthon_0th_TEAM10/src/main/java/com/example/skhuthon_0th_team10/domain/Review.java
@@ -0,0 +1,13 @@
+package com.example.skhuthon_0th_team10.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    Long id;
+
+
+}
